a0932b4de8cb957403f299ba7fdd60cba8bf69bd
Shubham Sabhaya <shubhamsabhaya07@gmail.com>
2026-06-26 12:48:59 +0530

    expense.py

--- expense.py
+++ expense.py
@@ -44,7 +44,7 @@ def category_report():
     print()
 
 while True:
-    print("==== Expense Tracker ====")
+    print("Expense Tracker")
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Total Expense")
